b6b7bec6d41f684b9d73e73c986b2706ac1aa777
CWCTBOY <qkrdlswo98@gmail.com>
2022-11-04 01:44:05 +0900

    solved: solve white label error && revise application.yml -> setting done! No error

--- src/main/java/dann/devlog_be/config/SwaggerConfig.java
+++ src/main/java/dann/devlog_be/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package dann.devlog_be.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -8,13 +9,14 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@Configuration
 public class SwaggerConfig {
   @Bean
   public Docket api(){
     return new Docket(DocumentationType.OAS_30)
       .useDefaultResponseMessages(false)
       .select()
-      .apis(RequestHandlerSelectors.basePackage("gdsc.blog.controller")) // 각자 패키지에 알맞게
+      .apis(RequestHandlerSelectors.basePackage("dann.devlog_be.controller")) // 각자 패키지에 알맞게
       .paths(PathSelectors.any())
       .build()
       .apiInfo(apiInfo());
--- src/main/java/dann/devlog_be/controller/TestController.java
+++ src/main/java/dann/devlog_be/controller/TestController.java
@@ -3,9 +3,17 @@ package dann.devlog_be.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
+  @ResponseBody
+  @GetMapping("/")
+  public String home() {
+    return "Home page!";
+  }
+
+  @ResponseBody
   @GetMapping("/hello")
   public String hello() {
     return "Hello World!";
